aa6c10612f50d9fa0a25c33c23778f393a470792
rodrigogborgia <rodrigo@gamificagroup.com>
2025-11-11 21:00:37 -0300

    frontend ya está

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-
-// Injected content via Sentry wizard below
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  output: 'standalone',
+  // Si usás imágenes externas, configurá aquí:
+  // images: { domains: ['cdn.heygen.com'] },
+};
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
-  module.exports,
+  nextConfig,
   {
     // For all available options, see:
     // https://www.npmjs.com/package/@sentry/webpack-plugin#options
@@ -27,18 +28,12 @@ module.exports = withSentryConfig(
     widenClientFileUpload: true,
 
     // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
     tunnelRoute: "/monitoring",
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
+    // Enables automatic instrumentation of Vercel Cron Monitors.
     automaticVercelMonitors: true,
   }
 );
